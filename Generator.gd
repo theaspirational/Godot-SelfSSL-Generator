81ab8001ea15ae4530f447d1f63c3b4d29d5d6a3
Vasyl Dovhanchyn <48733736+theaspirational@users.noreply.github.com>
2021-05-07 08:57:41 +0300

    added starting_date/ending_date

--- Generator.gd
+++ Generator.gd
@@ -6,23 +6,25 @@ var key_file_name = "my-ss-ssl-Key.key"
 
 onready var path = "user://Cert/"
 
-var CN = "FootballGame" # server name
-var O = "TowerGames" # org name
-var C = "UKR" # country iso 3166 code
+var CN = "myserver" # server name
+var O = "mygreatcompany" # org name
+var C = "UKR" # country iso-3166 code
 
+var starting_date = "20210501000000"
+var ending_date = "20220501000000"
 
 func _ready() -> void:
 	var D = Directory.new()
 	if not D.dir_exists(path):
 		D.make_dir(path)
-	_create_cert()
+	_generate_cert()
 	print("Cert created")
 
 
-func _create_cert():
+func _generate_cert():
 	var CNOC = "CN="+CN+",O="+O+",C="+C
 	var crypto = Crypto.new()
 	var key = crypto.generate_rsa(4096)
-	var cert = crypto.generate_self_signed_certificate(key, CNOC)
+	var cert = crypto.generate_self_signed_certificate(key, CNOC, starting_date, ending_date)
 	cert.save(path+cert_filename) 
 	key.save(path+key_file_name)
